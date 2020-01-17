fcc778e0ab9e07ac848dbf80c1f049aad9ffa3d7
niddumulu <36333121+niddumulu@users.noreply.github.com>
2020-01-17 12:46:45 +0700

    Update edit.php

--- admin/edit.php
+++ admin/edit.php
@@ -12,7 +12,7 @@ include("../koneksi.php"); // memanggil file koneksi.php untuk koneksi ke databa
 			
 			<?php
 			$nim = $_GET['nim']; // assigment nim dengan nilai nim yang akan diedit
-			$sql = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE nim='$nim'"); // query untuk memilih entri data dengan nilai nim terpilih
+			$sql = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE nim='$nim'"); // query untuk memilih entri data dengan nilai nim terpilih
 			if(mysqli_num_rows($sql) == 0){
 				header("Location: index.php");
 			}else{
@@ -26,7 +26,7 @@ include("../koneksi.php"); // memanggil file koneksi.php untuk koneksi ke databa
 				$kelas	    	 = $_POST['kelas'];
 				//$walikelas	     = $_POST['walikelas'];
 				
-				$update = mysqli_query($koneksi, "UPDATE tbl_mahasiswa SET username='$username', level='$level', nama='$nama', jenis_kelamin='$jenis_kelamin', kelas='$kelas' WHERE nim='$nim'") or die(mysqli_error()); // query untuk mengupdate nilai entri dalam database
+				$update = mysqli_query($koneksi, "UPDATE tbl_siswa SET username='$username', level='$level', nama='$nama', jenis_kelamin='$jenis_kelamin', kelas='$kelas' WHERE nim='$nim'") or die(mysqli_error()); // query untuk mengupdate nilai entri dalam database
 				if($update){ // jika query update berhasil dieksekusi
 					header("Location: edit.php?nim=".$nim."&pesan=sukses"); // tambahkan pesan=sukses pada url
 				}else{ // jika query update gagal dieksekusi
