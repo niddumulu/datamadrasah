caa40c9a5b3779637bf1d8ffe0475a91fbce1e22
niddumulu <36333121+niddumulu@users.noreply.github.com>
2020-01-17 12:31:23 +0700

    Update cari.php

--- admin/cari.php
+++ admin/cari.php
@@ -12,7 +12,7 @@ include("../koneksi.php"); // memanggil file koneksi.php untuk koneksi ke databa
 			<hr />
 			
 			<?php
-			$sql = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE nim='$nim'"); // query untuk memilih entri dengan nim terpilih
+			$sql = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE nim='$nim'"); // query untuk memilih entri dengan nim terpilih
 			if(mysqli_num_rows($sql) == 0){
 				header("Location: no_result.php");
 			}else{
@@ -20,7 +20,7 @@ include("../koneksi.php"); // memanggil file koneksi.php untuk koneksi ke databa
 			}
 			
 			if(isset($_GET['aksi']) == 'delete'){ // jika tombol 'Hapus Data' ditekan
-				$delete = mysqli_query($koneksi, "DELETE FROM tbl_mahasiswa WHERE nim='$nim'"); // query delete entri dengan nim terpilih
+				$delete = mysqli_query($koneksi, "DELETE FROM tbl_siswa WHERE nim='$nim'"); // query delete entri dengan nim terpilih
 				if($delete){ // jika query delete berhasil dieksekusi
 					echo '<div class="alert alert-danger alert-dismissable">><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Data berhasil dihapus.</div>'; // maka tampilkan 'Data berhasil dihapus.'
 				}else{ // jika query delete gagal dieksekusi
