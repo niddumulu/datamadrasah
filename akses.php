aa924949808ea9408dd42a2162215e8460dd0c0f
niddumulu <36333121+niddumulu@users.noreply.github.com>
2020-01-17 12:44:59 +0700

    Update akses.php

--- akses.php
+++ akses.php
@@ -6,6 +6,9 @@ if(isset($_SESSION['admin'])){ // jika session adalah admin
 	echo '<script language="javascript">document.location="admin/index.php";</script>'; // maka arahkan ke halaman admin
 }
 elseif(isset($_SESSION['siswa'])){ // jika session adalah user
-	echo '<script language="javascript">document.location="siswa/index.php";</script>'; // maka arahkan ke halaman user
+	echo '<script language="javascript">document.location="siswa/index.php";</script>'; // maka arahkan ke halaman siswa
+}
+elseif(isset($_SESSION['walikelas'])){ // jika session adalah user
+	echo '<script language="javascript">document.location="wali/index.php";</script>'; // maka arahkan ke halaman wali kelas
 }
 ?>
