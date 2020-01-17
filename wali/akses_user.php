d7bfed6432c463641ebfd92ac8d0930600c2cb3b
niddumulu <36333121+niddumulu@users.noreply.github.com>
2020-01-17 13:03:09 +0700

    Update akses_user.php

--- wali/akses_user.php
+++ wali/akses_user.php
@@ -2,7 +2,7 @@
 if (session_status() == PHP_SESSION_NONE) { // mengecek apakah session belum dimulai
     session_start(); // maka mulai session
 }
-if(!isset($_SESSION['user'])){ // jika session yg sedang berjalan bukan user
+if(!isset($_SESSION['walikelas'])){ // jika session yg sedang berjalan bukan user
 	echo '<script language="javascript">document.location="../login.php";</script>'; // maka diarahkan ke halaman login
 }
 ?>
