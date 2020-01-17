08a0445e261ea5e6d56eff3b33c62e8c1d791d5f
niddumulu <36333121+niddumulu@users.noreply.github.com>
2020-01-17 12:27:53 +0700

    Update rekap.php

--- admin/rekap.php
+++ admin/rekap.php
@@ -8,16 +8,16 @@ include("../koneksi.php"); // memanggil file koneksi.php untuk koneksi ke databa
 ?>
 <?php
 	$username = $_SESSION['admin'];
-	$sqlwakel = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE level='walikelas' "); // query jika filter dipilih					
-	$sqlwakelpr = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE level='walikelas' AND jenis_kelamin='Perempuan' "); // query jika filter dipilih					
-	$sqlwakellk = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE level='walikelas' AND jenis_kelamin='Laki-laki' "); // query jika filter dipilih
+	$sqlwakel = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE level='walikelas' "); // query jika filter dipilih					
+	$sqlwakelpr = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE level='walikelas' AND jenis_kelamin='Perempuan' "); // query jika filter dipilih					
+	$sqlwakellk = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE level='walikelas' AND jenis_kelamin='Laki-laki' "); // query jika filter dipilih
 	$countwakel = mysqli_num_rows($sqlwakel);
 	$countwakelpr = mysqli_num_rows($sqlwakelpr);
 	$countwakellk = mysqli_num_rows($sqlwakellk);
 	echo "Jumlah Wali Kelas : $countwakel, Terdiri dari : $countwakellk laki-laki dan $countwakelpr perempuan <br>";
 
 	$username = $_SESSION['admin'];
-	$sql = mysqli_query($koneksi, "SELECT * FROM tbl_mahasiswa WHERE level='siswa' "); // query jika filter dipilih					
+	$sql = mysqli_query($koneksi, "SELECT * FROM tbl_siswa WHERE level='siswa' "); // query jika filter dipilih					
 	$count = mysqli_num_rows($sql);
 	echo "Jumlah Siswa : $count";
 ?>	
